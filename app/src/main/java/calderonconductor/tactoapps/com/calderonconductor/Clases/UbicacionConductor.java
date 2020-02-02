b208072db26ee87a6974274a0efb88d3954b4d75
eperdomo86 <edward.perdomo@outlook.com>
2020-02-02 14:25:49 -0300

    Guardado ubicación del conductor
    Campos adicionales para el guardado de la ubicación del conductor.

--- app/src/main/java/calderonconductor/tactoapps/com/calderonconductor/Clases/UbicacionConductor.java
+++ app/src/main/java/calderonconductor/tactoapps/com/calderonconductor/Clases/UbicacionConductor.java
@@ -4,14 +4,30 @@ public class UbicacionConductor {
 
     private double lat;
     private double lon;
+    private String matricula;
+    private String color;
+    private String marca;
+    private String asignadoPor;
+    private String estado;
+    private String ofertadaATerceros;
+    private String referencia;
+    private String fechaHora;
 
     public UbicacionConductor(){
 
     }
 
-    public UbicacionConductor(double Lat, double Lon){
+    public UbicacionConductor(double Lat, double Lon, String Matricula, String Color, String Marca, String AsignadaPor, String Estado, String OfertadaATerceros, String Referencia, String FechaHora){
         this.setLat(Lat);
         this.setLon(Lon);
+        this.setMatricula(Matricula) ;
+        this.setColor(Color);
+        this.setMarca(Marca);
+        this.setAsignadoPor(AsignadaPor);
+        this.setEstado(Estado);
+        this.setOfertadaATerceros(OfertadaATerceros);
+        this.setReferencia(Referencia);
+        this.setFechaHora(FechaHora);
     }
 
     public double getLat() {
@@ -30,4 +46,67 @@ public class UbicacionConductor {
         this.lon = lon;
     }
 
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getAsignadoPor() {
+        return asignadoPor;
+    }
+
+    public void setAsignadoPor(String asignadoPor) {
+        this.asignadoPor = asignadoPor;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getOfertadaATerceros() {
+        return ofertadaATerceros;
+    }
+
+    public void setOfertadaATerceros(String ofertadaATerceros) {
+        this.ofertadaATerceros = ofertadaATerceros;
+    }
+
+    public String getReferencia() {
+        return referencia;
+    }
+
+    public void setReferencia(String referencia) {
+        this.referencia = referencia;
+    }
+
+    public String getFechaHora() {
+        return fechaHora;
+    }
+
+    public void setFechaHora(String fechaHora) {
+        this.fechaHora = fechaHora;
+    }
 }
--- app/src/main/java/calderonconductor/tactoapps/com/calderonconductor/Comandos/ComandoUbicacionConductor.java
+++ app/src/main/java/calderonconductor/tactoapps/com/calderonconductor/Comandos/ComandoUbicacionConductor.java
@@ -2,6 +2,8 @@ package calderonconductor.tactoapps.com.calderonconductor.Comandos;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import calderonconductor.tactoapps.com.calderonconductor.Clases.UbicacionConductor;
@@ -12,9 +14,18 @@ public class ComandoUbicacionConductor {
         final FirebaseDatabase database = FirebaseDatabase.getInstance();
         final DatabaseReference ref3 = database.getReference("ubicacionConductor");
 
-        Map<String, Double> ubicacion = new HashMap<>();
+        Map<String, Object> ubicacion = new HashMap<>();
         ubicacion.put("lat", ubicacionConductor.getLat());
         ubicacion.put("lon", ubicacionConductor.getLon());
+        ubicacion.put("asignadoPor", ubicacionConductor.getAsignadoPor());
+        ubicacion.put("color", ubicacionConductor.getColor());
+        ubicacion.put("estado", ubicacionConductor.getEstado());
+        ubicacion.put("marca", ubicacionConductor.getMarca());
+        ubicacion.put("matricula", ubicacionConductor.getMatricula());
+        ubicacion.put("ofertadaATerceros", ubicacionConductor.getOfertadaATerceros());
+        ubicacion.put("referencia", ubicacionConductor.getReferencia());
+        ubicacion.put("fechaActualizacion", ubicacionConductor.getFechaHora());
+
         ref3.child(idConductor).setValue(ubicacion);
     }
 }
